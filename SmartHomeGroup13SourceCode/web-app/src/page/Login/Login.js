49592a59baea8bca512adfaa8bd479bde2c5e91b
minhpham2k2 <aminh252781@gmail.com>
2023-10-30 20:03:18 +0700

    login

--- SmartHomeGroup13SourceCode/web-app/src/page/Login/Login.js
+++ SmartHomeGroup13SourceCode/web-app/src/page/Login/Login.js
@@ -2,16 +2,19 @@ import { Button, TextField } from '@mui/material';
 import styles from './Login.scss';
 import classNames from 'classnames/bind';
 import Checkbox from '@mui/material/Checkbox';
+import { useFormControl } from '@mui/material/FormControl';
+
 
 const cx = classNames.bind(styles);
 function Login() {
     return (
         <div className={cx('wrapper')}>
+            <img src = {require('../../assets/img/home.png')} alt='BG' className={cx('background')}/>
             <div className={cx('form')}>
-                <h2 className={cx('title')}> Log In</h2>
+                <h2 className={cx('title')}> LOG IN</h2>
                 <div className={cx('input')}>
-                    <TextField label="Usename" fullWidth variant="outlined" size = 'normal'/>
-                    <TextField label="Password" fullWidth  variant="outlined" size = 'normal' margin='normal'/>
+                    <TextField className={cx('username-input')} label="Usename" fullWidth variant="filled" size = 'normal'/>
+                    <TextField label="Password" className={cx('password-input')}  fullWidth  variant="filled" size = 'normal' margin="normal"/>
                 </div>
                 <Button className={cx('btn-login')} variant="contained" disableElevation size = 'small'>
                     Login
@@ -20,7 +23,7 @@ function Login() {
                     <div className={cx('checkbox')}> 
                         <Checkbox label = "Remember Me"  defaultChecked/>Remember Me
                     </div>
-                    <a href = '/'>Forgot Password</a>
+                    <a href = '/' className={cx('forgot')}>Forgot Password</a>
                 </div>
             </div>
         </div>
